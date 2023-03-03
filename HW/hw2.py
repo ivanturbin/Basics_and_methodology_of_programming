2166fa48fff6aaf91e6d0dfdc34822ffb84e47a0
ivanturbin <ivanturbin2004@gmail.com>
2023-03-03 21:15:40 +0300

    1

--- HW/hw2.py
+++ HW/hw2.py
@@ -1,6 +1,5 @@
 from random import randint
  
-#20 случайных чисел в интервале [0:100]
-random_list = [randint(0, 9999) for _ in range(randint(50, 100))]
- 
+random_list = [randint(0, 9999) for i in range(randint(50, 100))]
+    
 print(random_list)
