d429000647e252e26cd53977e52dcb528b06f6c1
ivanturbin <ivanturbin2004@gmail.com>
2023-06-02 18:30:13 +0300

    Update hw_17.py

--- HW/hw_17.py
+++ HW/hw_17.py
@@ -1,21 +1,20 @@
 import requests
-from urllib.parse import quote
+import urllib.parse
+import random
+
 def season_events(number_of_month, year):
-    month_names = ['январь', 'февраль', 'март', 'апрель', 'май', 'июнь',
-                   'июль', 'август', 'сентябрь', 'октябрь', 'ноябрь', 'декабрь']
-    month_name = month_names[number_of_month-1]
-    url = f'https://ru.wikipedia.org/wiki/Категория:{month_name}_{year}_года'
-    page = requests.get(url).text
+    month_names = ['январь', 'февраль', 'март', 'апрель', 'май', 'июнь', 'июль', 'август', 'сентябрь', 'октябрь', 'ноябрь', 'декабрь']
+    month_name = month_names[number_of_month - 1]
+    url = f'https://ru.wikipedia.org/wiki/{urllib.parse.quote(f"Категория:{month_name.capitalize()}_{year}_года")}'
+    response = requests.get(url)
     articles = []
-    for line in page.split('\n'):
-        if '<a href="/wiki/' in line:
-            title_start = line.find('title="') + len('title="')
-            title_end = line.find('"', title_start)
-            title = line[title_start:title_end]
-            articles.append(title)
-            if len(articles) == 5:
-                break
+    for line in response.text.split('\n'): 
+        if '<a href="/wiki' in line:
+            article_title = line.split('title="')[1].split('"')[0]
+            articles.append(article_title)
+    articles = random.sample(articles, min(len(articles), 5))
+    events = ' '.join([f'{i + 1}. {a}' for i, a in enumerate(articles)])
+    
     with open('wiki.txt', 'w') as f:
-        f.write(f'Вы родились в {month_name} в {year} году. ' \
-                f'{", ".join(articles)}')
-season_events(5, 2004)
+        f.write(f'Вы родились в {month_name} в {year} году. {events}')
+season_events(11, 1999) # создаст файл wiki.txt с описанием событий для ноября 1999 года
